89e3a0114e68251b9e91a04d6f1139653f107509
VishalMandala <vishalrao.mandala@gmail.com>
2023-04-11 21:28:48 -0400

    24th commit

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -6,8 +6,6 @@ import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
 import EmailIcon from "@mui/icons-material/Email";
 import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
-import TrafficIcon from "@mui/icons-material/Traffic";
-import LineChart from "../../components/LineChart";
 import BarChart from "../../components/BarChart";
 import GeographyChart from "../../components/GeographyChart";
 import StatBox from "../../components/StatBox";
@@ -159,6 +157,21 @@ const Dashboard = () => {
 
             {/* Row 3 */}
 
+            
+
+            <Box
+                gridColumn="span 4"
+                gridRow="span 2"
+                backgroundColor={colors.primary[400]}
+            >
+                <Typography variant="h4" fontWeight="600" sx={{ p:"30px 30px 0 30px"}}>
+                    Sales Quantity
+                </Typography>
+                <Box height="250px" mt="-20px">
+                    <BarChart isDashboard={true} />
+                </Box>
+            </Box>   
+
             <Box
                 gridColumn="span 4"
                 gridRow="span 2"
@@ -177,9 +190,23 @@ const Dashboard = () => {
                         Including Expenditures and Miscallaneous Expenses
                     </Typography>
                 </Box>
-            </Box>            
+            </Box>         
 
+            <Box
+                gridColumn="span 4"
+                gridRow="span 2"
+                backgroundColor={colors.primary[400]}
+                p="30px"
+            >
+                <Typography variant="h4" fontWeight="600" sx={{ mb: "15px"}}>
+                    Pie Chart
+                </Typography>
+                <Box height="250px" mt="-20px">
+                    <PieChart isDashboard={true} />
+                </Box>
+            </Box> 
 
+            {/*  */}
         </Box>
     </Box>
     );
